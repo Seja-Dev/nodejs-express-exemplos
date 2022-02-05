3fbb8c7ba1628c9f6bff1bb67091c071192ddd2a
josehfortes <jose@seja.dev>
2022-02-05 10:07:43 -0300

    Criado tratativa de erros para signup

--- src/modules/user/userService.js
+++ src/modules/user/userService.js
@@ -4,11 +4,8 @@ const getUserByEmail = (searchEmail) =>
   users.find((obj) => obj.email === searchEmail)
 
 export const signup = (data) => {
-  if (getUserByEmail(data.email)) {
-    console.log("EXISTE O EMAIL")
-  } else {
-    users.push(data)
-  }
+  if (getUserByEmail(data.email)) throw new Error('email_existente')
 
+  users.push(data)
   return true
 }
